9d6e39ee6f2d650f9acce3cc264a956a03885911
Helen Spencer-Wallace <Caverna@Helen-Spencer-Wallaces-MacBook-Pro-1358.local>
2021-03-11 17:11:25 -1000

    fixed for loop pop

--- sketch.js
+++ sketch.js
@@ -73,7 +73,7 @@ function draw() {
 
   // camera rotation 
 
-  camera(0, 20 + sin(frameCount * (0.05)) * 10, 200 + sin(frameCount * 0.001) * 3000, 0, 0, 0, 0, 1, 0);
+  camera(0, 20 + sin(frameCount * (0.05)) * 10, 200 + sin(frameCount * 0.01) * 3000, 0, 0, 0, 0, 1, 0);
 
   // -z pink sky
   push();
@@ -148,8 +148,9 @@ function draw() {
   for (let i = 0; i < shells.length; i++) {
     shells[i].move();
     shells[i].display();
-  pop();
   }
+  pop();
+  
 // handle saving the frame
   // console.log('capturing frame');
   // capturer.capture(canvas);
